627689a9d1a54bf1a833d0ec43991e895360f4ad
nouhailaghanjiri <nouhailaghanjiri@gmail.com>
2024-11-30 14:47:57 +0100

    Charger dynamiquement les commentaires

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import ArticleList from './Components/ArticleList'
-import CommentForm from './Components/CommentForm'
+import React, { useState, useEffect } from "react";
+import ArticleList from "./Components/ArticleList";
+import CommentForm from "./Components/CommentForm";
+import CommentList from "./Components/CommentList"; 
+const App = () => {
+  const [comments, setComments] = useState([]);
 
-function App() {
+  useEffect(() => {
+    fetch("/data/comments.json")
+      .then((response) => response.json())
+      .then((data) => setComments(data))
+      .catch((error) => console.error("Erreur de chargement :", error));
+  }, []);
+
   return (
     <div>
       <ArticleList />
       <CommentForm />
+      { }
+      <CommentList comments={comments} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
