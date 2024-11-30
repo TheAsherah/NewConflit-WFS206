f2895e862ad23d55f767e1b76603561bad704f4b
AichaAyoub1999 <jabrizinebai@gmail.com>
2024-11-30 15:05:43 +0100

    ajouter un bouton supprimer pour retirer un commentaire

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import ArticleList from './Components/ArticleList'
 import CommentForm from './Components/CommentForm'
+import CommentList from './Components/CommentList'
 
 function App() {
+  const [comments, setComments] = useState([]);
+
+  const deleteComment = (index) => {
+    setComments((prevComments) => prevComments.filter((_, i) => i !== index));
+  };
+
+  const addComment = (newComment) => {
+    setComments((prevComments) => [...prevComments, newComment]);
+  };
+
+
   return (
     <div>
       <ArticleList />
-      <CommentForm />
+      <CommentForm addComment={addComment} />
+      <CommentList comments={comments} deleteComment={deleteComment} />
     </div>
   )
 }
